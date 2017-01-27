11b186c724b2a18816a5d48da0d5215132ba98c1
Natalie Baker <ohcodinglady@gmail.com>
2017-01-27 05:39:32 +0000

    import external scripts

--- js/main.js
+++ js/main.js
@@ -1,33 +1,58 @@
 var portfoliosDir = 'https://filename-array-generator-nataliebaker.c9users.io/portfolios';
 var gallery = {
-	workers: [],
+	imports: [],
 	portfolios: []
-}
+};
 
 // get auto-generated page 
 $.ajax({url: portfoliosDir}).then(function(html) {
     // create temporary DOM element
-    var document = $(html);
+    var doc = $(html);
     
     // find all links in the page
-    document.find('a').each(function() {
+    doc.find('a').each(function() {
         var URL = $(this).attr('href');
 
-        // add each url to the `workers` array
-        gallery.workers.push(URL);
+        // add each url to the `imports` array
+        gallery.imports.push(URL);
     });
-    console.log(gallery.workers);
+    
+    gallery.runImports = function() {
+    	for (var i = 5; i < gallery.imports.length; i++) {
+    		var URL = '../portfolios/' + gallery.imports[i];
+    		$.getScript(URL.toString(), function(){
+				gallery.portfolios.push(newPortfolio);
+				console.log(newPortfolio);
+				var createMenu = function() {
+					var newOption = '<option value="%value%">%name%</option>';
+					for (var i = 0; i < gallery.portfolios.length; i++) {
+						var optionName = newOption.replace("%name%", gallery.portfolios[i].name);
+						$('#list').append(optionName.replace("%value%", gallery.portfolios[i].oldURL));
+					}
+				};
+				createMenu();
+				
+				gallery.displayPortfolio = function() {
+					document.getElementById('display').innerHTML = '';
+					var iframe = document.createElement('iframe');
+					iframe.src = document.getElementById('list').value;
+					document.getElementById('display').appendChild(iframe);
+					console.log(iframe.src);
+				};
+			});
+    	}
+    	
+		console.log(gallery.portfolios);
+    };
+	gallery.runImports();
+	/*gallery.displayPortfolio = function() {
+		document.getElementById('display').innerHTML = '';
+		var iframe = document.createElement('iframe');
+		iframe.src = document.getElementById('list').value;
+		document.getElementById('display').appendChild(iframe);
+		console.log(iframe.src);
+	};*/
 });
-
-gallery.menuDisplay = function() {
-	for (var i = 5; i < gallery.workers.length; i++) {
-		var newOption = document.createElement('option');
-		var optionName = document.createTextNode(gallery.workers[i]);
-		newOption.appendChild(optionName);
-		document.getElementById('list').append(newOption);
-	}
-};
-gallery.menuDisplay();
 /*
 gallery.display = function() {
 
