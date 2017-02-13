acd6fef942fccbc2e8163a0dc89b1a25fa814b2a
Natalie Baker <ohcodinglady@gmail.com>
2017-02-12 22:11:12 -0600

    add code comments

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,32 @@
+//all our dependencies
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var path = require('path');
-var directory = './portfolios/';
 
-
-
+//our display function grabs the files merged from each contributor, reads them, and pushes the data to an array that we use to build the page
 function display() {
+	var directory = './portfolios/';
 	var fileList = [];
 	var portfolios = [];
+	//get the list of files we will be using
 	fs.readdir(directory, function(err, files) {
 		if (err) return;
 		files.forEach(function(f) {
 			fileList.push(f);
 		});
-		//console.log(fileList);
+		//read each file and convert it to json
 		fileList.forEach(function(p) {
 			var file = directory + p;
-			//console.log(file);
 			var portfolio = JSON.parse(fs.readFileSync(file, 'utf8'));
-			//console.log(portfolio.name);
 			portfolios.push(portfolio);
 		})
-		console.log(portfolios);
+		//generate the page, iterate the information from the files in the page
 		router.get('/', function(req, res, next) {
-	
-	res.render('index', { title: 'Portfolio Gallery', portfolios: portfolios });
-	
-
-});
+			res.render('index', { title: 'Portfolio Gallery', portfolios: portfolios });
+		});
 	});
 }
-//console.log(portfolios);
-
-// get home page
-
+//set it all in motion!
 display();
 module.exports = router;
